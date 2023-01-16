156df5902cffd8c07130bfc4f2eaf883b67ead97
M2Busto <121911586+M2Busto@users.noreply.github.com>
2023-01-16 18:33:47 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let ResultString = "0" ; let ResultNumber = 0
+const Elements = document.querySelectorAll(".element")
+const Result_Box = document.querySelector("#result-box")
+
+Elements.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (button.innerHTML === "=") {
+            ResultNumber = eval(ResultString)
+            ResultString = ResultNumber
+            Result_Box.innerHTML = ResultNumber
+            console.log(ResultNumber)
+        } else if (button.innerHTML === "C") {
+            Result_Box.innerHTML = "0"
+            ResultString = "0" ; ResultNumber = 0
+        } 
+        else {
+            if (ResultString === "0") {
+                ResultString = ""
+            }
+            ResultString += button.innerHTML
+            Result_Box.innerHTML = ResultString
+        }
+    })
+})
